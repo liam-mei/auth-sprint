7aa260ab845af02782e994dc51fa67b99856df73
Derrick Mei <derrick.mei.168@gmail.com>
2020-01-25 03:11:21 -0600

    /login completed with session and token support
    token support can also use cookies to send token if wanted but is currently commented out

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -27,7 +27,14 @@ router.post("/login", async (req, res, next) => {
       // Session Implementation
       // req.session.user = user;
       // res.json({message: `Welcome ${username}`})
+
       // Token Implementation
+      const token = jwt.sign(user, secrets.secret, { expiresIn: "7d" });
+      // If we want to use cookie for token instead of having user save it
+      // res.cookie('token', token, { maxAge: 1000 * 60 * 60 * 24 * 7 })
+      res.json({ message: `Welcome ${username}`, token });
+    } else {
+      next({ status: 401, error: "Invalid Credentials" });
     }
   } catch (err) {
     next(err);
