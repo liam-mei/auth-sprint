c756dd76d3c0531b1ac486d87103d9703666bec5
Derrick Mei <derrick.mei.168@gmail.com>
2020-01-25 01:58:03 -0600

    changed userModel to use async bcrypt method

--- database/userModel.js
+++ database/userModel.js
@@ -17,13 +17,24 @@ function findOneBy(filter) {
     .first();
 }
 
-async function addOne(user) {
-  const salt = await bcrypt.genSalt(14);
-  const hashedPassword = await bcrypt.hash(user.password, salt);
-  user.password = hashedPassword;
+function addOne(user) {
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) next(err);
+    bcrypt.hash(user.password, salt, async function(err, hashedPassword) {
+      if (err) next(err);
+      user.password = hashedPassword;
+      const [id] = await db("users").insert(user);
+      return findOneBy({ id });
+    });
+  });
 
-  const [id] = await db("users").insert(user);
-  return findOneBy({ id });
+  // Other Async Method
+  // const salt = await bcrypt.genSalt(14);
+  // const hashedPassword = await bcrypt.hash(user.password, salt);
+  // user.password = hashedPassword;
+
+  // const [id] = await db("users").insert(user);
+  // return findOneBy({ id });
 }
 
 async function updateOne(user) {
