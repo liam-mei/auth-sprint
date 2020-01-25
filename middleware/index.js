cedce8dd9feea1ee891ef814adf10afb6386c9ee
Derrick Mei <derrick.mei.168@gmail.com>
2020-01-25 02:13:08 -0600

    added middleware to check username and password exists
    also check if username unique

--- middleware/index.js
+++ middleware/index.js
@@ -1,16 +1,36 @@
+const userModel = require("../database/userModel");
+
 function sanityCheck(req, res, next) {
   res.json({ message: "I work" });
 }
+
 function wrongRoute(req, res, next) {
   res.status(401).json({ error: "Route does not exist" });
 }
+
 function errorHandler(err, req, res, next) {
   console.log("Global Error: ", err);
   res.status(err.status || 500).json({ message: err.message, err });
 }
 
+function checkUsernamePasswordExists(username, password) {
+  if (!username || !password)
+    next({ status: 400, message: "username and password required" });
+}
+
+async function checkUsernameUnique(username) {
+  const user = await userModel.findOneBy({ username });
+  if (user)
+    next({
+      status: 400,
+      message: `Username ${username} already exists, please chose another name`
+    });
+}
+
 module.exports = {
   sanityCheck,
   wrongRoute,
-  errorHandler
+  errorHandler,
+  checkUsernamePasswordExists,
+  checkUsernameUnique
 };
