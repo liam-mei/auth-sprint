aa4ce00ec85992809f6b745315c7a5fd482fdbdb
Derrick Mei <derrick.mei.168@gmail.com>
2020-01-25 01:37:31 -0600

    added middleware and comments to server.js

--- api/server.js
+++ api/server.js
@@ -1,18 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+// Import Dependencies
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
+// Import Routers
+const authRouter = require("../auth/auth-router.js");
+const jokesRouter = require("../jokes/jokes-router.js");
 
+// Import Middleware
+const authenticate = require("../auth/authenticate-middleware.js");
+const { sanityCheck, wrongRoute, errorHandler } = require("../middleware");
+
+// Use Middleware
 const server = express();
-
 server.use(helmet());
 server.use(cors());
+server.use(morgan("dev"));
 server.use(express.json());
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+// Use Routers
+server.use("/api/auth", authRouter);
+server.use("/api/jokes", authenticate, jokesRouter);
+
+// Use Custom Middleware
+server.get("/", sanityCheck);
+server.use(wrongRoute);
+server.use(errorHandler);
 
 module.exports = server;
