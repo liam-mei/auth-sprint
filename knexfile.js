7135115ed7d408ee46cb86e17002fc1ce662601f
Derrick Mei <derrick.mei.168@gmail.com>
2020-02-02 01:23:36 -0600

    added production config for postgres

--- knexfile.js
+++ knexfile.js
@@ -1,49 +1,33 @@
-module.exports = {
-  development: {
-    client: "sqlite3",
-    connection: { filename: "./database/auth.db3" },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./database/migrations",
-      tableName: "dbmigrations"
-    },
-    seeds: { directory: "./database/seeds" }
+// This was the Guided Project's way of being DRY
+const migrationsAndSeeds = {
+  migrations: {
+    directory: "./database/migrations"
   },
-  test: {
-    client: "sqlite3",
-    connection: { filename: "./database/test.db3" },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./database/migrations",
-      tableName: "dbmigrations"
-    },
-    seeds: { directory: "./database/seeds" }
+  seeds: {
+    directory: "./database/seeds"
   }
 };
 
-// This was the Guided Project's way of being DRY
-// const sqlite = {
-//   client: "sqlite3",
-//   useNullAsDefault: true,
-//   migrations: {
-//     directory: "./data/migrations",
-//   },
-//   seeds: {
-//     directory: "./data/seeds",
-//   },
-// }
-
-// module.exports = {
-//   dev: {
-//     ...sqlite,
-//     connection: {
-//       filename: "./data/dev.db3",
-//     },
-//   },
-//   test: {
-//     ...sqlite,
-//     connection: {
-//       filename: "./data/test.db3",
-//     },
-//   },
-// }
+module.exports = {
+  development: {
+    ...migrationsAndSeeds,
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/db_development.db3"
+    }
+  },
+  test: {
+    ...migrationsAndSeeds,
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/db_test.db3"
+    }
+  },
+  production: {
+    ...migrationsAndSeeds,
+    client: "pg",
+    connection: process.env.DATABASE_URL
+  }
+};
